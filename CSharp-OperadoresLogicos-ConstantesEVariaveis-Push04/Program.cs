300c9b91feacb8ddbe8b8f10113cfbbbf86c0ad9
Lethícia Santos <leticia-santos75@hotmail.com>
2022-02-17 19:40:59 -0300

    Adicionar arquivos de projeto.

---
+++ CSharp-OperadoresLogicos-ConstantesEVariaveis-Push04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_OperadoresLogicos_ConstantesEVariaveis_Push04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
